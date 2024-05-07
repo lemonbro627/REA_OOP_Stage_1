b87aeae851a73dc98796dda593b00b6ad291488f
Dmitrii Bazhin <me@lemonbro.ru>
2024-05-07 16:24:47 +0500

    add Save & Load

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Библиотека = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Библиотека.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Библиотека
+            // 
+            this.Библиотека.Controls.Add(this.tabPage1);
+            this.Библиотека.Controls.Add(this.tabPage2);
+            this.Библиотека.Controls.Add(this.tabPage3);
+            this.Библиотека.Controls.Add(this.tabPage4);
+            this.Библиотека.Controls.Add(this.tabPage5);
+            this.Библиотека.Location = new System.Drawing.Point(0, 28);
+            this.Библиотека.Name = "Библиотека";
+            this.Библиотека.SelectedIndex = 0;
+            this.Библиотека.Size = new System.Drawing.Size(960, 499);
+            this.Библиотека.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(952, 473);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Книги";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(952, 473);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Книги у читателей";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(952, 473);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Читатели";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(952, 473);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Читатели в залах";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(952, 473);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Читальные залы";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(49, 22);
+            this.toolStripDropDownButton1.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(960, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(443, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(503, 461);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список книг";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(8, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(429, 161);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Создание книги";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(960, 528);
+            this.Controls.Add(this.Библиотека);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Библиотека";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Библиотека.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TabControl Библиотека;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,12 @@ namespace REA_OOP_Stage_1
 {
     public partial class Form1 : Form
     {
+        List<Book> books = new List<Book>();
+        List<BookToReader> booksToReaders = new List<BookToReader>();
+        List<Reader> readers = new List<Reader>();
+        List<ReaderToHall> readerToHalls = new List<ReaderToHall>();
+        List<Hall> halls = new List<Hall>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +27,93 @@ namespace REA_OOP_Stage_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Сохранение...");
+            string fname;
+            BinaryFormatter bf;
+            FileStream fs;
+            //Save Book
+            fname = "data_book.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            bf.Serialize(fs, books);
+            fs.Close();
+
+            //Save BookToReaders
+            fname = "data_bookToReaders.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            bf.Serialize(fs, booksToReaders);
+            fs.Close();
+
+            //Save Reader
+            fname = "data_readers.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            bf.Serialize(fs, readers);
+            fs.Close();
+
+            //Save ReaderToHall
+            fname = "data_readersToHalls.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            bf.Serialize(fs, readerToHalls);
+            fs.Close();
+
+            //Save Hall
+            fname = "data_halls.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            bf.Serialize(fs, halls);
+            fs.Close();
+            Console.WriteLine("Сохранено");
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Загрузка...");
+            string fname;
+            BinaryFormatter bf;
+            FileStream fs;
+            //Save Books
+            fname = "data_book.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            books = (List<Book>)bf.Deserialize(fs);
+            fs.Close();
+
+            //Save Books
+            fname = "data_bookToReaders.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            booksToReaders = (List<BookToReader>)bf.Deserialize(fs);
+            fs.Close();
+
+            //Save Books
+            fname = "data_readers.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            readers = (List<Reader>)bf.Deserialize(fs);
+            fs.Close();
+
+            //Save Books
+            fname = "data_readersToHalls.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            readerToHalls = (List<ReaderToHall>)bf.Deserialize(fs);
+            fs.Close();
+
+            //Save Books
+            fname = "data_halls.bin"; // прописываем путь к файлу
+            bf = new BinaryFormatter();
+            fs = new FileStream(fname, FileMode.OpenOrCreate);
+            halls = (List<Hall>)bf.Deserialize(fs);
+            fs.Close();
+            Console.WriteLine("Загружено");
         }
     }
 }
